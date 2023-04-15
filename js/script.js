e113250b87fccad5765f83d29aae70d9715ee9b8
Zaira <zairacruz1981@gmail.com>
2023-04-15 15:18:14 -0300

    Aula 2 git
    Testando pull request

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ var nome = document.getElementById("nome");
 var tel  = document.getElementById("tel");
 var email = document.getElementById("email");
 
+//Zaira esteve aqui
+
 div = document.getElementById("btnEnviar")
 div.addEventListener("click", ()=>{ 
 
